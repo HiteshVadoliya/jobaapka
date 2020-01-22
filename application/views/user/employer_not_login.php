c7de83e565f9ed246e277654535078ba0060993b
HiteshVadoliya <hmvadoliya.iipl2013@gmail.com>
2020-01-23 00:56:39 +0530

    employer

--- application/views/user/employer_not_login.php
+++ application/views/user/employer_not_login.php
@@ -5,20 +5,29 @@
          <div class="row">
             <div class="col-md-12">
                <div class="breadcromb-box">
-                  <h3>WELCOME TO JOBAAPKA</h3>
+                  <h3>EMPLOYER</h3>
                </div>
             </div>
          </div>
       </div>
    </div>
+   
+     <div class="breadcromb-box-pagin my_breadcum">
+        <ul>
+           <li>QUICK ACCOUNT CREATION WITH LITTLE INFO</li>
+           <li>MOST AFFORDABLE</li>
+           <li>MORE THAN JUST HIRING SERVICES</li>
+        </ul>
+     </div>
+   
    <div class="breadcromb-bottom">
       <div class="container">
          <div class="row">
             <div class="col-md-12">
                <div class="breadcromb-box-pagin">
                   <ul>
-                     <li><a href="<?= base_url(); ?>">home</a></li>
-                  </ul>
+                     <li><a href="<?= base_url(); ?>">Home</a></li>
+                  </ul>                  
                </div>
             </div>
          </div>
@@ -75,13 +84,97 @@
                      <div class="col-lg-12 col-md-12">
                         <div class="single-browse-company single-browse-company-custom" style="background: none;">                                 
                            <h3>
-                           <a class="button plan_button " href="javascript:;" data-toggle="modal" data-target="#jobseeker_plan_modal">GROW YOUR BUSINESS WITH US</a>
+                           <!-- <a class="button plan_button " href="javascript:;" data-toggle="modal" data-target="#jobseeker_plan_modal">GROW YOUR BUSINESS WITH US</a> -->
+                           <a class="button plan_button section_1_click" href="javascript:;">GROW YOUR BUSINESS WITH US</a>
+                           <a class="button plan_button section_2_click" href="javascript:;">MANAGE YOUR BUSINESS EASILY</a>
                            </h3>
                         </div>
+                     </div>                   
+                  </div>
+               </div>
+            </div>
+
+            <div class="tab-content" id="section_2_click">
+               <div class="tab-pane fade show active" id="company_a" role="tabpanel" aria-labelledby="company_a_tab">
+                  <div class="row">                           
+                     <div class="col-md-4 companyBox moreBox">
+                        <div class="single-browse-company ">
+                          <table class="table table-responsive plan_table_1" cellspacing="0" cellpadding="0">
+                            <tr><td><h5>Employee Attendance Tracking</h5></td></tr>
+                            <tr><td class="plan_1_text">Easy to use tool</td></tr>
+                            <tr><td class="plan_1_text">Records Leaves and Generates Salary slips</td></tr>
+                            <tr><td class="plan_1_text plan_1_text_color">Lifetime validity</td></tr>
+                            <tr>
+                            <td class="single-login-field">
+                              <button class="plan_know_more plan_3_action" type="button" name="" value="know more" data-toggle="modal" data-target="#plan_first_popup">GET A FREE DEMO</button>
+                              </td>
+                            </tr>
+                          </table>
+                        </div>
                      </div>
+                     <div class="col-md-4 companyBox moreBox">
+                        <div class="single-browse-company ">
+                          <table class="table table-responsive plan_table_1" cellspacing="0" cellpadding="0">
+                            <tr><td><h5>Billing/Quotation Tool</h5></td></tr>
+                            <tr><td class="plan_1_text">Print Invoices Quickly</td></tr>
+                            <tr><td class="plan_1_text">Get Revenue Report</td></tr>
+                            <tr><td class="plan_1_text plan_1_text_color">Lifetime validity</td></tr>
+                            <tr>
+                            <td class="single-login-field">
+                              <button class="plan_know_more plan_3_action" type="button" name="" value="know more" data-toggle="modal" data-target="#plan_first_popup">GET A FREE DEMO</button>
+                            </td>
+                            </tr>
+                          </table>
+                        </div>
+                     </div>
+                     <div class="col-md-4 companyBox moreBox">
+                        <div class="single-browse-company ">
+                          <table class="table table-responsive plan_table_1" cellspacing="0" cellpadding="0">
+                            <!-- <tr><td><h5>Employee Attendance Tracking</h5></td></tr> -->
+                            <tr><td class="plan_1_text">GET COMBO OFFER <br>ATTENDANCE TRACKING <br> + <br>BILLING TOOL</td></tr>
+                            <tr>
+                            <td class="single-login-field">
+                              <button class="plan_know_more plan_3_action" type="button" name="" value="know more" data-toggle="modal" data-target="#plan_first_popup">GET A FREE DEMO</button>
+                            </td>
+                          </tr>
+                          </table>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
 
-
-                                              
+            <div class="tab-content" id="section_1_click">
+               <div class="tab-pane fade show active" id="company_a" role="tabpanel" aria-labelledby="company_a_tab">
+                  <div class="row">                           
+                     <div class="col-md-6 col-md-offset-2 companyBox moreBox" style="margin: 0 auto;">
+                        <div class="single-browse-company single-browse-company-custom">
+                          <table class="table table-responsive plan_table_1">
+                              <tr>
+                                <td><h4>Most affordable and unique <br><?= $plan['period']; ?> month plan</h4></td>
+                                <td></td>
+                              </tr>
+                              <tr>
+                                <td class="plan_1_text">Unlimited CV Download/Access</td>
+                                <td><i class="fa fa-check icon"></i></td>
+                              </tr>
+                              <tr>
+                                <td class="plan_1_text">Post unlimited jobs</td>
+                                <td><i class="fa fa-check icon"></i></td>
+                              </tr>
+                              <tr>
+                                <td class="plan_1_text">One Free technology consulting for your business issues</td>
+                                <td><i class="fa fa-check icon"></i></td>
+                              </tr>
+                              <tr>
+                                <td class="single-login-field">
+                                  <a href="<?= base_url('employer_plan'); ?>" class="plan_know_more" type="button" name="" value="know more">know more</a>
+                                  </td>
+                                <td></td>
+                              </tr>
+                          </table>
+                        </div>
+                     </div>
                   </div>
                </div>
             </div>
@@ -93,6 +186,77 @@
 </section>
 
 
+<div class="modal fade" id="plan_first_popup" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body ">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <div class="single-resume-feild feild-flex-2">
+                       <div class="single-input">
+                          <label for="name">Organization name :</label>
+                          <input type="text" value="<?= $jobseeker_data['fname']; ?>" id="fname" name="fname">
+                       </div>
+                    </div>
+                    <div class="single-resume-feild feild-flex-2">
+                       <div class="single-input">
+                          <label for="name">Concerned person name :</label>
+                          <input type="text" value="<?= $jobseeker_data['fname']; ?>" id="fname" name="fname">
+                       </div>
+                    </div>
+                    <div class="single-resume-feild feild-flex-2">
+                       <div class="single-input">
+                          <label for="name">Email id :</label>
+                          <input type="text" value="<?= $jobseeker_data['fname']; ?>" id="fname" name="fname">
+                       </div>
+                    </div>
+                    <div class="single-resume-feild feild-flex-2">
+                       <div class="single-input">
+                          <label for="name">Contact number :</label>
+                          <input type="text" value="<?= $jobseeker_data['fname']; ?>" id="fname" name="fname">
+                       </div>
+                    </div>
+                    <div class="single-resume-feild feild-flex-2">
+                       <div class="single-input">
+                          <label for="name">Suitable Date :</label>
+                          <input  class="datepicker" type="text" id="job_date" name="job_date" value="" autocomplete="off">
+                       </div>
+                    </div>
+                    <div class="single-resume-feild feild-flex-2">
+                       <div class="single-input">
+                          <label for="name">Suitable Time for Online Demo :</label>
+                          <select>
+                            <?php
+                            for ($i=1; $i < 25 ; $i++) { 
+                              ?>
+                              <option value="<?= $si ?>"><?= $i; ?></option>
+                              <?php
+                            }
+                            ?>
+                          </select>
+                       </div>
+                    </div>
+                    
+                </div>                
+            </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php /*
 <div class="modal fade" id="jobseeker_plan_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -140,9 +304,23 @@
     </div>
   </div>
 </div>
+*/ ?>
 
 <script type="text/javascript">
-  $('#jobseeker_plan_modal').on('shown.bs.modal', function () {
+  $('.plan_3_action').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
+  /*$('#jobseeker_plan_modal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })*/
+</script>
+<script type="text/javascript">
+  $(".section_2_click").on("click",function(){
+    $("#section_1_click").css('display','none');
+    $("#section_2_click").fadeToggle( "slow", "linear" );;
+  });
+  $(".section_1_click").on("click",function(){
+    $("#section_2_click").css('display','none');
+    $("#section_1_click").fadeToggle( "slow", "linear" );;
+  });
 </script>
